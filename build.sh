5852d9fbaf73156cc2d2f35f82fd175a0d03de4b
ice <offical@byterain.co>
2025-11-02 20:07:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd build
 cmake .. -Dpybind11_DIR=$(python3 -m pybind11 --cmakedir)
 
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-  make -j$(nproc)
-else
   cmake --build build --config Release
+else
+  make -j$(nproc)
 fi
