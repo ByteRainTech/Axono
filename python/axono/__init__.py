91ce01bec0b0debd2db97d268a22752d6d759c7c
ice <offical@byterain.co>
2025-11-02 12:28:27 +0800

    Create __init__.py

---
+++ python/axono/__init__.py
@@ -0,0 +1,9 @@
+"""
+Axono - High Performance Computing Library
+"""
+
+from .core import *
+
+__version__ = "0.1.0"
+__author__ = "Axono Team"
+__description__ = "High performance computing library for big data processing"
