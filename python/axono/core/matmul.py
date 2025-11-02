cf79c8627b1a967d31d5e86445d23750987ebcc5
ice <offical@byterain.co>
2025-11-02 18:39:35 +0800

    feat: matmul

---
+++ python/axono/core/matmul.py
@@ -0,0 +1,9 @@
+"""
+Axono Matmul
+"""
+from core import matmul as _matmul
+from .tensor import Tensor
+
+def matmul(a: Tensor, b: Tensor) -> Tensor:
+    raw_result = _matmul(a._tensor, b._tensor)
+    return Tensor.from_raw(raw_result)
