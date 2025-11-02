cac3f2d06bb0e52e0783a28cd694513f2c1e047c
ice <offical@byterain.co>
2025-11-02 15:41:33 +0800

    Update test_memory_copy.py

--- python/tests/core/test_memory_copy.py
+++ python/tests/core/test_memory_copy.py
@@ -1,22 +1,22 @@
-import sys
+# import sys
 
-sys.path.append("../../")
+# sys.path.append("../../")
 
-import unittest
+# import unittest
 
-import numpy as np
+# import numpy as np
 
-from axono.core import memory_copy
-
-
-class TestMemoryCopy(unittest.TestCase):
-    def test_memory_copy(self):
-        src = np.array([1, 2, 3, 4, 5], dtype=np.int32)
-        dst = np.zeros_like(src)
-        print(f"Before: src={src}, dst={dst}")
-        memory_copy(dst, src)
-        print(f"After:  src={src}, dst={dst}")
+# from axono.core import memory_copy
 
 
-if __name__ == "__main__":
-    unittest.main()
+# class TestMemoryCopy(unittest.TestCase):
+#     def test_memory_copy(self):
+#         src = np.array([1, 2, 3, 4, 5], dtype=np.int32)
+#         dst = np.zeros_like(src)
+#         print(f"Before: src={src}, dst={dst}")
+#         memory_copy(dst, src)
+#         print(f"After:  src={src}, dst={dst}")
+
+
+# if __name__ == "__main__":
+#     unittest.main()
