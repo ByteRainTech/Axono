493825c5d6b325055a6eefc0936dceefdee5fe90
cxykevin <87745710+cxykevin@users.noreply.github.com>
2025-11-02 21:08:54 +0800

    Add support for Python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     package_data={
         "axono": ["library/*.so", "library/*.dll", "library/*.dylib"],
