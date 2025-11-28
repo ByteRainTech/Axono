9c289e59ce031d96d3bb5e38ec6e73052074a15e
ice <offical@byterain.co>
2025-11-28 15:59:36 +0800

    Update __init__.py

--- python/axono/core/__init__.py
+++ python/axono/core/__init__.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-library_path = os.path.dirname(os.path.dirname(__file__)) + "/library/"
+library_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + "/library/"
 sys.path.append(library_path)
 
-from core import DataType, Status
+from axonolib import DataType, Status
 
 from . import operators
 from .tensor import Tensor
