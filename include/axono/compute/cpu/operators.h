5983e027ea44fc3ec503ece704438b58433179ea
ice <offical@byterain.co>
2025-11-28 16:12:50 +0800

    Update operators.h

--- include/axono/compute/cpu/operators.h
+++ include/axono/compute/cpu/operators.h
@@ -7,22 +7,23 @@
 namespace axono {
 namespace compute {
 namespace cpu {
-
+namespace operators {
 // 基础内存操作
-AXONO_EXPORT Status MemoryCopy(const Context &ctx, void *dst, const void *src,
+AXONO_EXPORT core::Status MemoryCopy(const core::Context &ctx, void *dst, const void *src,
                                size_t num_bytes);
 
-AXONO_EXPORT Status MemorySet(const Context &ctx, void *dst, int value,
+AXONO_EXPORT core::Status MemorySet(const core::Context &ctx, void *dst, int value,
                               size_t num_bytes);
 // 矩阵喵~
-AXONO_EXPORT Status MatMul(const Context &ctx, const Tensor &a, const Tensor &b,
-                           Tensor &result);
+AXONO_EXPORT core::Status MatMul(const core::Context &ctx, const core::Tensor &a, const core::Tensor &b,
+                           core::Tensor &result);
 
-AXONO_EXPORT Status Add(const Context &ctx, const Tensor &a, const Tensor &b,
-                        Tensor &result);
+AXONO_EXPORT core::Status Add(const core::Context &ctx, const core::Tensor &a, const core::Tensor &b,
+                        core::Tensor &result);
 
-AXONO_EXPORT Status AddScalar(const Context &ctx, const Tensor &a, void *scalar,
-                              size_t scalar_size, Tensor &result);
-} // namespace cpu
-} // namespace compute
-} // namespace axono
+AXONO_EXPORT core::Status AddScalar(const core::Context &ctx, const core::Tensor &a, void *scalar,
+                              size_t scalar_size, core::Tensor &result);
+}
+}
+}
+}
