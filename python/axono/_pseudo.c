04590592dfec8bafcbad8acf607aceb52f3f9c69
ice <offical@byterain.co>
2025-12-12 19:41:56 +0800

    Update _pseudo.c

--- python/axono/_pseudo.c
+++ python/axono/_pseudo.c
@@ -1,1 +1,16 @@
+/* _pseudo.c : dummy extension module for setuptools platform tagging */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
+static PyModuleDef pseudomodule = {
+    PyModuleDef_HEAD_INIT,
+    "_pseudo",              /* m_name  */
+    "dummy module",         /* m_doc   */
+    -1,                     /* m_size  */
+    NULL, NULL, NULL, NULL, NULL
+};
+
+PyMODINIT_FUNC PyInit__pseudo(void)
+{
+    return PyModule_Create(&pseudomodule);
+}
