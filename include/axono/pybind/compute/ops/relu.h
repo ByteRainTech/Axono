07d7118d6ecfd0a36dfd3605a9bfce22f8a03c6a
ice <offical@byterain.co>
2026-01-03 12:44:09 +0800

    Refactor ReLU operations and registration

--- include/axono/pybind/compute/ops/relu.h
+++ include/axono/pybind/compute/ops/relu.h
@@ -3,55 +3,65 @@
 namespace py = pybind11;
 
 #include "axono/core/tensor.h"
+#include "axono/core/ops.h"
 
 #ifdef COMPILED_WITH_CUDA
 #include "axono/compute/cuda/ops/relu.h"
 #endif
 #include "axono/compute/cpu/ops/relu.h"
 
-void init_relu_operations(py::module &m) {
-  m.def(
-      "relu",
-      [](const axono::core::Tensor &input) {
-        axono::core::Context ctx;
-        axono::core::Tensor output =
-            axono::core::Tensor(input.dtype(), input.shape(), input.device());
+namespace axono {
+namespace compute {
+namespace ops {
 
-        axono::core::Status status;
-        if (input.is_cuda()) {
+py::object op_impl_relu(const py::args& args);
+py::object op_impl_relu_(const py::args& args);
+
+REGISTER_OP(relu) {
+    core::Context ctx;
+    core::Tensor result;
+    core::Status status;
+    if (args.size() != 1) {
+        throw std::runtime_error("执行 add 需要传入 1 个 Tensor 喵~");
+    }
+    
+    auto& input = pybind11::cast<core::Tensor&>(args[0]);
+    core::Tensor output(input.dtype(), input.shape(), input.device());
+    
+    if (input.is_cuda()) {
 #ifdef COMPILED_WITH_CUDA
-          status = axono::compute::cuda::ops::Relu(ctx, input, output);
+        status = cuda::ops::Relu(ctx, input, output);
 #endif
-        } else {
-          status = axono::compute::cpu::ops::Relu(ctx, input, output);
-        }
+    } else {
+        status = cpu::ops::Relu(ctx, input, output);
+    }
+    if (status != core::Status::OK)
+        throw std::runtime_error("执行 ReLU 时出现问题，错误代码：" + std::to_string(static_cast<int>(status)));
 
-        if (status != axono::core::Status::OK) {
-          throw std::runtime_error("喵！ReLU计算时发生错误，错误代码: " +
-                                   std::to_string(static_cast<int>(status)));
-        }
-        return output;
-      },
-      "ReLU activation function", py::arg("input"),
-      py::return_value_policy::move),
-
-      m.def(
-          "relu_",
-          [](axono::core::Tensor &tensor) {
-            axono::core::Context ctx;
-            axono::core::Status status;
-            if (tensor.is_cuda()) {
+    return pybind11::cast(output);
+}
+REGISTER_OP(relu_) {
+    core::Context ctx;
+    core::Tensor result;
+    core::Status status;
+    if (args.size() != 1) {
+        throw std::runtime_error("执行 add 需要传入 1 个 Tensor 喵~");
+    }
+    
+    auto& tensor = pybind11::cast<core::Tensor&>(args[0]);
+    
+    if (tensor.is_cuda()) {
 #ifdef COMPILED_WITH_CUDA
-              status = axono::compute::cuda::ops::ReluInplace(ctx, tensor);
+        status = cuda::ops::ReluInplace(ctx, tensor);
 #endif
-            } else {
-              status = axono::compute::cpu::ops::ReluInplace(ctx, tensor);
-            }
-            if (status != axono::core::Status::OK) {
-              throw std::runtime_error("喵！InplaceReLU 出现错误！");
-            }
+    } else {
+        status = cpu::ops::ReluInplace(ctx, tensor);
+    }
+    if (status != core::Status::OK)
+        throw std::runtime_error("执行 ReLU 时出现问题，错误代码：" + std::to_string(static_cast<int>(status)));
 
-            return tensor;
-          },
-          "Inplace ReLU activation function", py::arg("tensor"));
+    return pybind11::cast(tensor);
+}
+}
+}
 }
