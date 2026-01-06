7dfd25401c987125291717c7c3c304bc4fc4089e
ice <offical@byterain.co>
2026-01-06 22:02:09 +0800

    Update and rename include/axono/compute/cuda/ops/relu.h to include/axono/ops/cuda/relu.h

--- include/axono/ops/cuda/relu.h
+++ include/axono/ops/cuda/relu.h
@@ -1,11 +1,12 @@
+#pragma once
+
 #include "axono/core/macros.h"
 #include "axono/core/tensor.h"
 #include "axono/core/types.h"
 
 namespace axono {
-namespace compute {
-namespace cuda {
 namespace ops {
+namespace cuda {
 
 core::Status DispatchRelu(const core::Tensor &input, core::Tensor &output);
 core::Status DispatchReluInplace(core::Tensor &tensor);
@@ -13,7 +14,6 @@ core::Status Relu(const core::Context &ctx, const core::Tensor &input,
                   core::Tensor &output);
 core::Status ReluInplace(const core::Context &ctx, core::Tensor &tensor);
 
-}  // namespace ops
 }  // namespace cuda
-}  // namespace compute
+}  // namespace ops
 }  // namespace axono
